e1cff7138bc3df34be88f12f43a070e631c95f5f
Hailey Lee <81673151+sleeping-psystudent@users.noreply.github.com>
2022-06-08 23:06:23 +0800

    Update hw1.py

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -31,6 +31,40 @@ def CeilingandFloor (i):
 
  # 基底中央數
 def find_middle(n):
+    """(10 pts) Find the Middle Line of the Tree
+    
+    In this part, you have to find the middle line of the tree and return the distance from the middle line to 
+    left end.
+    For example, if the input number is 2, you have to find the middle line of a level two tree and return the
+    distance to left end that is 10.
+
+            /*\
+           /***\
+          /*****\
+       /***********\
+      /*************\
+     /***************\
+    /********|********\
+    1234567891
+             0
+    
+    Similarly, if the input number is 3, you have to return 17.
+
+                   /*\
+                  /***\
+                 /*****\
+              /***********\
+             /*************\
+            /***************\
+           /*****************\
+        /***********************\
+       /*************************\
+      /************|||************\
+     /*************|||*************\
+    /**************|||**************\
+    12345678911111111
+             01234567
+    """
     layer = CeilingandFloor(n)
     return int((layer[1]+3)/2)
     pass
@@ -64,7 +98,54 @@ def content(i, j, n, m):
         return ' '
 
 # 組裝good，填空good
-def print_level_n_tree(n):        
+def print_level_n_tree(n):
+     """(50 pts) Print Out Level N Tree
+    
+    In this part, you have to print the level n tree which n is a integer.
+    You can use 'hw1_example.py' to take a look what a level n tree looks like.
+    For example, if the input number is 2, you have to print out a level two tree, and it will looks like:
+ 
+            /*\
+           /***\
+          /*****\
+       /***********\
+      /*************\
+     /***************\
+    /********|********\
+    
+    If the input number is 5, your tree will looks like:
+
+                                      /*\
+                                     /***\
+                                    /*****\
+                                 /***********\
+                                /*************\
+                               /***************\
+                              /*****************\
+                           /***********************\
+                          /*************************\
+                         /***************************\
+                        /*****************************\
+                       /*******************************\
+                   /***************************************\
+                  /*****************************************\
+                 /*******************************************\
+                /*********************************************\
+               /***********************************************\
+              /*************************************************\
+          /*********************************************************\
+         /***********************************************************\
+        /****************************|||||****************************\
+       /*****************************|||||*****************************\
+      /******************************|||@|******************************\
+     /*******************************|||||*******************************\
+    /********************************|||||********************************\
+
+    Image that n is a big number. You tree must be huge.
+    !!! Notice that your result must the same as what 'hw1_example.py' print out. !!!
+
+
+    """
     m = 1
     
     # 圖高
@@ -83,6 +164,21 @@ def print_level_n_tree(n):
 
 
 def print_level_two_tree():
+    """(10 pts) Print Out Level Two Tree
+
+    You have to print out a level two tree in this part.
+    If you don't have what a level two tree look like, you can use 'hw1_example.py' to generate a level two tree.
+    It will look like this:
+    
+            /*\
+           /***\
+          /*****\
+       /***********\
+      /*************\
+     /***************\
+    /********|********\
+
+    """
     return print_level_n_tree(2)
     pass
 
@@ -112,6 +208,11 @@ def forK(p, g, a, b):
 
 # 組裝讚讚
 def Diffie_Hellman_cracker(PublicPrime, PublicNum, PrivateA, PrivateB):
+    """(30 pts) Diffie Hellman cracker
+    In this part, you need to implement a function which can enter four numbers as 
+    parameters and find out the private key a ,b and common number K then return those numbers.
+    
+    """
     a = fora(PublicPrime, PublicNum, PrivateA)
     b = forb(PublicPrime, PublicNum, PrivateB)
     K = forK(PublicPrime, PublicNum, a, b)
