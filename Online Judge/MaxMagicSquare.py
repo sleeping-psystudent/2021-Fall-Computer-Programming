a3ac923d39c3b114769a05a2d06bb328cecc308a
Tsai-Rung Lee <81673151+sleeping-psystudent@users.noreply.github.com>
2023-06-18 17:10:19 +0800

    Create MaxMagicSquare.py

---
+++ Online Judge/MaxMagicSquare.py
@@ -0,0 +1,20 @@
+def solution(A):
+    column = len(A)
+    row = max(len(A[0]),len(A[len(A)-1]))
+    
+    for rows in A:
+        if len(rows)!=row:
+            rows.append(0)
+
+    for i in range(min(column, row), 0, -1): # Column或Row比較小的
+        for j in range(column-i+1): # 上下
+            for m in range(row-i+1): # 左右
+                # 對角線
+                # print([A[j+c][m+c] for c in range(i)], [A[j+c][m+i-c-1] for c in range(i)])
+                if sum([A[j+c][m+c] for c in range(i)])==sum([A[j+c][m+i-c-1] for c in range(i)]):
+                    diagonal = sum([A[j+c][m+c] for c in range(i)])
+                    # 橫線和直線
+                    if sum(A[j][m:m+i])==sum([row[m] for row in A[j:j+i]]):
+                        if (sum(A[j][m:m+i])==diagonal):
+                            return i
+    pass
